5e4b44119e432fa77a059b04f93a5c624f54c051
Outerwolf <camilo.ch2701@outlook.com>
2023-11-25 22:57:29 -0500

    rename folders

--- Src/WebApi/Program.cs
+++ Src/WebApi/Program.cs
@@ -7,16 +7,11 @@ var config = builder.Configuration;
 builder.Configuration.AddJsonFile("appsettings.Development.json",
     optional: true,
     reloadOnChange: true);
-// Add services to the container.
 
 builder.Services.AddSecurityAuthApi();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSecurityAuthApplication();
 builder.Services.AddSecurityAuthInfrastructure(config);
-
-// builder.Services.AddApplication();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
